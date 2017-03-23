315d1cb3c404139a35e2b3253bd302a1e3151998
Taylor Laine <tlaine@msudenver.edu>
2017-03-22 21:22:04 -0600

    adds chocice toggle and title style changes

--- js/main.js
+++ js/main.js
@@ -1,34 +1,54 @@
 console.log("im not crazy");
 
 let sqrs = document.getElementsByTagName("td");
-let choice;
+let choice = 'X';
 let gameover = false;
+let xActive = true;
+let oActive = false;
 const moves = [];
 
 function addClick() {
 	for (var i = 0; i < sqrs.length; i++) {
-    sqrs[i].addEventListener('click', function() {
-		console.log(this);
-		console.log(this.innerHTML);
-		// if (!this.innerHTML){
-		// 	this.innerHTML = "X";
-		// } else if (this.innerHTML === 'X'){
-		// 	this.innerHTML = "O";
-		// } else if (this.innerHTML === 'O') {
-		// 	this.innerHTML = "";
-		// }
-		this.innerHTML = choice;
+	    sqrs[i].addEventListener('click', function() {
+			console.log(this);
+			console.log(this.innerHTML);
+			// if (!this.innerHTML){
+			// 	this.innerHTML = "X";
+			// } else if (this.innerHTML === 'X'){
+			// 	this.innerHTML = "O";
+			// } else if (this.innerHTML === 'O') {
+			// 	this.innerHTML = "";
+			// }
+			this.innerHTML = choice;
+			moves.push(this.getAttribute('id'));
+			if (moves.length >= 3){
+				checkMatch();
+			}
+
 		});
 	}
 	document.getElementById("x-choice").addEventListener("click", function() {
 		console.log('x works');
 		choice = 'X';
 		console.log(choice + ' choice works');
+		xActive = true;
+		oActive = false;
+		if (xActive === true) {
+			this.style.backgroundColor = "blue";
+		} 
+		document.getElementById("o-choice").style.backgroundColor = "#ff4040";
+
 	});
 	document.getElementById("o-choice").addEventListener("click", function() {
 		console.log('o works');
 		choice = 'O';
 		console.log(choice + ' choice works');
+		xActive = false;
+		oActive = true;
+		if (oActive === true) {
+			this.style.backgroundColor = "blue";
+		}
+		document.getElementById("x-choice").style.backgroundColor = "#ff4040";
 	});
 
 
@@ -41,6 +61,7 @@ function checkMatch () {
 	if (sqrs[0].innerHTML==="X" && sqrs[1].innerHTML==="X" && sqrs[2].innerHTML==="X") {
 		console.log("x wins");
 		alert("X has one");
+		xWin = true;
 	} else if (sqrs[0].innerHTML==="O" && sqrs[1].innerHTML==="O" && sqrs[2].innerHTML==="O") {
 		console.log("O wins");
 		alert("O has one");
@@ -71,12 +92,27 @@ function checkMatch () {
 	}  else if (sqrs[2].innerHTML==="X" && sqrs[5].innerHTML==="X" && sqrs[8].innerHTML==="X") {
 	 	console.log("X wins");
 		alert("X has one");
+	} else if (sqrs[2].innerHTML==="O" && sqrs[5].innerHTML==="O" && sqrs[8].innerHTML==="O") {
+	 	console.log("O wins");
+		alert("O has one");
+	} else if (sqrs[0].innerHTML==="X" && sqrs[4].innerHTML==="X" && sqrs[8].innerHTML==="X") {
+	 	console.log("X wins");
+		alert("X has one");
+	} else if (sqrs[0].innerHTML==="O" && sqrs[4].innerHTML==="O" && sqrs[8].innerHTML==="O") {
+	 	console.log("O wins");
+		alert("O has one");
+	} else if (sqrs[2].innerHTML==="X" && sqrs[4].innerHTML==="X" && sqrs[6].innerHTML==="X") {
+	 	console.log("X wins");
+		alert("X has one");
+	} 
+	else if (sqrs[2].innerHTML==="O" && sqrs[4].innerHTML==="O" && sqrs[6].innerHTML==="O") {
+	 	console.log("O wins");
+		alert("O has one");
 	}
 }
 
 
 addClick();
-checkMatch();
 // sqrs.addEventListener("click", function() {
 // 	console.log(this);
 // });	
